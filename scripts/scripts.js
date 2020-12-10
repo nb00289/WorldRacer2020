e58c12d76621d124be76d8a59448fffb8fd13422
Nate Bender <natebender@nates-air.attlocal.net>
2020-12-10 09:54:43 -0800

    Rough outline for JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,49 @@
 WorldRacer2020 = {
-    end: function(){
-      let y = 3
-    },
-    
-    init: function() {
-      let x = 2
-      //i made an edit -Bo//
-    },
-    
-    start: function() {
-      // Hello
-    }
-  }
-  
-  MyApp.init();
+
+  // All names are changeable, just the outline to get everything in code
+  menu: document.getElementById("menu_button"),
+  start: document.getElementById("start_button"),
+  settings: document.getElementById("difficulty_button"),
+  level: document.getElementById("level_select_button"),
+  sound: document.getElementById("sound_button"),
+  music: document.getElementById("music_button"),
+  car: document.getElementById("user_car"),
+  track: document.getElementsByClassName("track"),
+
+
+  init: function () {
+    startRace();
+    collision();
+  },
+
+  startRace: function () {
+    startTimer();
+  },
+
+  finishRace: function () {
+    stopTimer();
+  },
+
+  startTimer: function () {
+
+  },
+
+  stopTimer: function () {
+
+  },
+
+  renderCar: function () {
+
+  },
+
+  moveCar: function () {
+
+  },
+
+  collision: function () {
+
+  },
+
+}
+
+WorldRacer2020.init();
