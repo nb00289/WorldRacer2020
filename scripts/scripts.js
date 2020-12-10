131f6e837926f55978057bc8d9fd23bc50ffe732
Nate Bender <natebender@nates-air.attlocal.net>
2020-12-10 10:25:16 -0800

    small JS changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,13 @@
 WorldRacer2020 = {
 
-  // All names are changeable, just the outline to get everything in code
+  // All names and id's are changeable, just the outline to get everything in code
   menu: document.getElementById("menu_button"),
   start: document.getElementById("start_button"),
   settings: document.getElementById("difficulty_button"),
   level: document.getElementById("level_select_button"),
   sound: document.getElementById("sound_button"),
   music: document.getElementById("music_button"),
-  car: document.getElementById("user_car"),
+  car: document, //either div or element,
   track: document.getElementsByClassName("track"),
 
 
