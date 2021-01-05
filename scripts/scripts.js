ffb9bc0a600ec8962fdb36f0e230b1fbf1ebd083
Nate Bender <natebender@nates-air.attlocal.net>
2021-01-05 12:53:15 -0800

    JS edits and Car functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,8 +7,8 @@ WorldRacer2020 = {
   sound: document.getElementById("soundBtn"),
   music: document.getElementById("musicBtn"),
 
-  car: null,
-  wall: [],
+  car: undefined,
+  track_container: document.getElementById("grid"),
   raceTrackArray1: [
     [0, 0, 2], [0, 1, 2], [0, 2, 2], [0, 3, 2], [0, 4, 2], [0, 5, 2], [0, 6, 2], [0, 7, 2], [0, 8, 2], [0, 9, 2], [0, 10, 2],
     [0, 11, 2], [0, 12, 2], [0, 13, 2], [0, 14, 2], [0, 15, 2], [0, 16, 2], [0, 17, 2], [0, 18, 2], [0, 19, 2], [0, 20, 2],
@@ -114,6 +114,8 @@ WorldRacer2020 = {
 
   init: function () {
     WorldRacer2020.renderTrack();
+    WorldRacer2020.createCar();
+    WorldRacer2020.renderCar();
   },
 
   timer: {
@@ -163,11 +165,11 @@ WorldRacer2020 = {
   createCar: function () {
     let cardiv = document.createElement("div");
     cardiv.className = "car";
-    this.track_container.append(cardiv);
+    this.track_container.appendChild(cardiv);
     let car = {
       immage: "",
-      x_pos: x,
-      y_pos: y,
+      x_pos: 200,
+      y_pos: 300,
       x_velocity: 3,
       y_velocity: 3,
       element: cardiv,
@@ -176,11 +178,8 @@ WorldRacer2020 = {
   },
 
   renderCar: function () {
-    for (let i = 0; i < this.people.length; i++) {
-      this.people[i].element.style.top = this.people[i].y_pos + "px";
-      this.people[i].element.style.left = this.people[i].x_pos + "px";
-      this.people[i].element.style.backgroundColor = this.people[i].color;
-    }
+    this.car.element.style.top = this.car.y_pos + "px";
+    this.car.element.style.left = this.car.x_pos + "px";
   },
 
   moveCar: function () {
@@ -193,27 +192,26 @@ WorldRacer2020 = {
   renderTrack: function () {
     // 0 = race track, 1 = wall, 2 = grass
     //for (i = 0; i < this.raceTrackArray; i++){}
-    let track_container = document.getElementById("grid")
     for (i = 0; i < this.raceTrackArray1.length; i++) {
       if (this.raceTrackArray1[i][2] === 0) {
         let raceSquare = document.createElement("div")
         raceSquare.className = "race_square";
-        track_container.appendChild(raceSquare);
+        this.track_container.appendChild(raceSquare);
       }
       if (this.raceTrackArray1[i][2] === 1) {
         let wallSquare = document.createElement("div")
         wallSquare.className = "wall_square";
-        track_container.appendChild(wallSquare);
+        this.track_container.appendChild(wallSquare);
       }
       if (this.raceTrackArray1[i][2] === 2) {
         let grassSquare = document.createElement("div")
         grassSquare.className = "grass_square";
-        track_container.appendChild(grassSquare);
+        this.track_container.appendChild(grassSquare);
       }
       if (this.raceTrackArray1[i][2] === 3) {
         let finishSquare = document.createElement("div")
         finishSquare.className = "finish_square";
-        track_container.appendChild(finishSquare)
+        this.track_container.appendChild(finishSquare)
       }
     }
     //this.raceSquare = track_container.children;
