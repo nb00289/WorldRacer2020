332351b81e5d194dd6c97e7a1e6c103e30fa46fb
Nate Bender <natebender@nates-air.attlocal.net>
2021-01-06 09:03:25 -0800

    Major function changes and additions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,10 +4,10 @@ WorldRacer2020 = {
   //menu: document.getElementById("menuBtn"),
   //startRacing: document.getElementById("startBtn"),
   //difficulty: document.getElementById("difficultyBtn"),
-  sound: document.getElementById("soundBtn"),
-  music: document.getElementById("musicBtn"),
+  //sound: document.getElementById("soundBtn"),
+  //music: document.getElementById("musicBtn"),
 
-  car: undefined,
+  carArray: [],
   track_container: document.getElementById("grid"),
   raceTrackArray1: [
     [0, 0, 2], [0, 1, 2], [0, 2, 2], [0, 3, 2], [0, 4, 2], [0, 5, 2], [0, 6, 2], [0, 7, 2], [0, 8, 2], [0, 9, 2], [0, 10, 2],
@@ -81,7 +81,7 @@ WorldRacer2020 = {
     [3, 1, 0], [3, 2, 0], [3, 3, 0], [3, 4, 0], [3, 5, 0], [3, 6, 0], [3, 7, 1], [3, 8, 1],
     //5
     [3, 1, 2], [3, 2, 1], [3, 3, 1], [3, 4, 0], [3, 5, 0], [3, 6, 0], [3, 7, 0], [3, 8, 0], [3, 9, 0], [3, 10, 0],
-    [2, 11, 0], [2, 12, 0], [2, 13, 0], [2, 14, 0], [2, 15, 0], [2, 16, 0], [2, 17, 0], [2, 8, 0], [2, 9, 0], [3, 0, 0],
+    [2, 11, 0], [2, 12, 0], [2, 13, 3], [2, 14, 0], [2, 15, 0], [2, 16, 0], [2, 17, 0], [2, 8, 0], [2, 9, 0], [3, 0, 0],
     [3, 1, 0], [3, 2, 0], [3, 3, 0], [3, 4, 0], [3, 5, 0], [3, 6, 0], [3, 7, 0], [3, 8, 0], [3, 9, 0], [3, 30, 0],
     [3, 1, 0], [3, 2, 0], [3, 3, 0], [3, 4, 0], [3, 5, 0], [3, 6, 1], [3, 7, 1], [3, 8, 2],
     //4
@@ -91,7 +91,7 @@ WorldRacer2020 = {
     [3, 1, 0], [3, 2, 0], [3, 3, 0], [3, 4, 0], [3, 5, 1], [3, 6, 1], [3, 7, 2], [3, 8, 2],
     //3
     [3, 1, 2], [3, 2, 2], [3, 3, 2], [3, 4, 1], [3, 5, 1], [3, 6, 0], [3, 7, 0], [3, 8, 0], [3, 9, 0], [3, 10, 0],
-    [2, 11, 0], [2, 12, 0], [2, 13, 0], [2, 14, 0], [2, 15, 0], [2, 16, 0], [2, 17, 0], [2, 8, 0], [2, 9, 0], [3, 0, 0],
+    [2, 11, 0], [2, 12, 0], [2, 13, 3], [2, 14, 0], [2, 15, 0], [2, 16, 0], [2, 17, 0], [2, 8, 0], [2, 9, 0], [3, 0, 0],
     [3, 1, 0], [3, 2, 0], [3, 3, 0], [3, 4, 0], [3, 5, 0], [3, 6, 0], [3, 7, 0], [3, 8, 0], [3, 9, 0], [3, 30, 0],
     [3, 1, 0], [3, 2, 0], [3, 3, 0], [3, 4, 1], [3, 5, 1], [3, 6, 2], [3, 7, 2], [3, 8, 2],
     //2
@@ -111,52 +111,22 @@ WorldRacer2020 = {
     [0, 31, 2], [0, 32, 2], [0, 33, 2], [0, 34, 2], [0, 35, 2], [0, 36, 2], [0, 37, 2],
   ],
 
+  raceTrackArray2: [
+    [[0, 0, 2], [0, 1, 2], [0, 2, 2], [0, 3, 2], [0, 4, 2], [0, 5, 2], [0, 6, 2], [0, 7, 2], [0, 8, 2], [0, 9, 2], [0, 10, 2],
+    [0, 11, 2], [0, 12, 2], [0, 13, 2], [0, 14, 2], [0, 15, 2], [0, 16, 2], [0, 17, 2], [0, 18, 2], [0, 19, 2], [0, 20, 2],
+    [0, 21, 2], [0, 22, 2], [0, 23, 2], [0, 24, 2], [0, 25, 2], [0, 26, 2], [0, 27, 2], [0, 28, 2], [0, 29, 2], [0, 30, 2],
+    [0, 31, 2], [0, 32, 2], [0, 33, 2], [0, 34, 2], [0, 35, 2], [0, 36, 2], [0, 37, 2],]
+  ],
+
 
   init: function () {
     WorldRacer2020.renderTrack();
-    WorldRacer2020.createCar();
-    WorldRacer2020.renderCar();
+    //WorldRacer2020.createCar();
+    //WorldRacer2020.renderCar();
   },
 
   timer: {
-    //This was taken off of :
-    //https://joe-riggs.com/blog/2012/05/javascript-count-up-timer-with-hours-minutes-second-hours-minutes/
-    //It said it required jquery, but this is way to "count up"
-    time: 0,
-    isRunning: false,
-    start: function () {
-      var time_shown = $("#realtime").text();
-      var time_chunks = time_shown.split(":");
-      var hour, mins, secs;
-
-      hour = Number(time_chunks[0]);
-      mins = Number(time_chunks[1]);
-      secs = Number(time_chunks[2]);
-      secs++;
-      if (secs == 60) {
-        secs = 0;
-        mins = mins + 1;
-      }
-      if (mins == 60) {
-        mins = 0;
-        hour = hour + 1;
-      }
-      if (hour == 13) {
-        hour = 1;
-      }
-
-      $("#realtime").text(hour + ":" + plz(mins) + ":" + plz(secs));
-
-    }, plz: function (digit) {
-
-      var zpad = digit + '';
-      if (digit < 10) {
-        zpad = "0" + zpad;
-      }
-      return zpad;
-    }
   },
-  //Need to make it understandable and change it to work for vanilla JS
 
   countdown: {
     // when track is loaded in, timer visable on screen counds down from # (10?)
@@ -164,7 +134,9 @@ WorldRacer2020 = {
 
 
   init: function () {
-    renderTrack();
+    WorldRacer2020.renderTrack();
+    WorldRacer2020.createCar();
+    //WorldRacer2020.renderCar();
   },
 
   createCar: function () {
@@ -172,7 +144,6 @@ WorldRacer2020 = {
     cardiv.className = "car";
     this.track_container.appendChild(cardiv);
     let car = {
-      immage: "",
       x_pos: 200,
       y_pos: 300,
       x_velocity: 3,
@@ -192,11 +163,14 @@ WorldRacer2020 = {
 
   //if car hits track - maybe be relpaced with distance
   collision: function () {
+    //for (i = 0; i < this.carArray; i++){
+      //for (i = 0; i < this.raceTrackArray1; i++){
+        //if (this.car){
   },
 
   renderTrack: function () {
     // 0 = race track, 1 = wall, 2 = grass
-    //for (i = 0; i < this.raceTrackArray; i++){}
+    //if (this.raceTrackArray1 == true){
     for (i = 0; i < this.raceTrackArray1.length; i++) {
       if (this.raceTrackArray1[i][2] === 0) {
         let raceSquare = document.createElement("div")
@@ -219,6 +193,9 @@ WorldRacer2020 = {
         this.track_container.appendChild(finishSquare)
       }
     }
+    //if(raceTrackArray2 == true && raceTrackArray1 == false && 
+    //raceTrackArray3 == false){
+      
     //this.raceSquare = track_container.children;
     //this.wallSquare = track_container.children;
     //this.grassSquare = track_container.children;
