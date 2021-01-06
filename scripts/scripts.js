1140b6c8b9a45b7f34426d99c1adbe10d626e435
Nate Bender <natebender@nates-air.attlocal.net>
2021-01-06 09:55:58 -0800

    Car functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -112,10 +112,10 @@ WorldRacer2020 = {
   ],
 
   raceTrackArray2: [
-    [[0, 0, 2], [0, 1, 2], [0, 2, 2], [0, 3, 2], [0, 4, 2], [0, 5, 2], [0, 6, 2], [0, 7, 2], [0, 8, 2], [0, 9, 2], [0, 10, 2],
+    [0, 0, 2], [0, 1, 2], [0, 2, 2], [0, 3, 2], [0, 4, 2], [0, 5, 2], [0, 6, 2], [0, 7, 2], [0, 8, 2], [0, 9, 2], [0, 10, 2],
     [0, 11, 2], [0, 12, 2], [0, 13, 2], [0, 14, 2], [0, 15, 2], [0, 16, 2], [0, 17, 2], [0, 18, 2], [0, 19, 2], [0, 20, 2],
     [0, 21, 2], [0, 22, 2], [0, 23, 2], [0, 24, 2], [0, 25, 2], [0, 26, 2], [0, 27, 2], [0, 28, 2], [0, 29, 2], [0, 30, 2],
-    [0, 31, 2], [0, 32, 2], [0, 33, 2], [0, 34, 2], [0, 35, 2], [0, 36, 2], [0, 37, 2],]
+    [0, 31, 2], [0, 32, 2], [0, 33, 2], [0, 34, 2], [0, 35, 2], [0, 36, 2], [0, 37, 2],
   ],
 
 
@@ -136,7 +136,7 @@ WorldRacer2020 = {
   init: function () {
     WorldRacer2020.renderTrack();
     WorldRacer2020.createCar();
-    //WorldRacer2020.renderCar();
+    WorldRacer2020.renderCar();
   },
 
   createCar: function () {
@@ -195,7 +195,7 @@ WorldRacer2020 = {
     }
     //if(raceTrackArray2 == true && raceTrackArray1 == false && 
     //raceTrackArray3 == false){
-      
+
     //this.raceSquare = track_container.children;
     //this.wallSquare = track_container.children;
     //this.grassSquare = track_container.children;
